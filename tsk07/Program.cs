8678501f656a8385f62cd05f7f6d4f0b84e3ab25
Scorppb <scorppb@yandex.ru>
2022-08-10 17:36:03 +0300

    tsk07

---
+++ tsk07/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// System.Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 20,
+    xc = 80, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+System.Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+System.Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+System.Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 10;
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0){
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if (what == 1){
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if (what == 2){
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    System.Console.WriteLine("+");
+    count++;
+}
