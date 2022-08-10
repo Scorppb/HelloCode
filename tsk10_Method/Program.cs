d29a2e5a0d68f1cf9438376e9d93a9d36eb6ce1d
Scorppb <scorppb@yandex.ru>
2022-08-10 22:24:19 +0300

    tsk10

---
+++ tsk10_Method/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 21, 62, 53, 18, 34, 45, 76, 97, 18, 89 };
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
