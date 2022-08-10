5faadfa82fc977608fdc08a9920795bbfefcf3e5
Scorppb <scorppb@yandex.ru>
2022-08-10 16:57:29 +0300

    tsk05

--- tsk05/Program.cs
+++ tsk05/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Ввелите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+} 
+else 
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
